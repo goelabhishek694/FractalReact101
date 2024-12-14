a0d0af2ca6b7c5e9e67c7bdeeb652f53605d14f7
Abhishek <abhishekgoel@gofynd.com>
2024-12-14 21:35:00 +0530

    class, functional comp

---
+++ React101/firstApp/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Counter from './components/Counter'
+import CounterClass from './components/CounterClass'
+
+function App() {
+  return (
+    <>
+    {/* <Counter></Counter> */}
+    <CounterClass></CounterClass>
+    </>
+  )
+}
+
+export default App
