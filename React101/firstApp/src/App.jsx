4ae7784c620ecac1bb9979f6d04203e4b13733b4
Abhishek <abhishekgoel@gofynd.com>
2024-12-17 20:24:31 +0530

    forms,controlled,uncontrolled

--- React101/firstApp/src/App.jsx
+++ React101/firstApp/src/App.jsx
@@ -2,14 +2,25 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import Counter from './components/Counter'
-import CounterClass from './components/CounterClass'
+// import Counter from './components/Counter'
+// import CounterClass from './components/CounterClass'
+import SimpleForm from './components/SimpleForm'
+import TemperatureDisplay from './components/TemperatureDisplay'
+import TemperatureInput from './components/TemperatureInput'
 
 function App() {
+  //state updation will happen only where the state is defined 
+  const [temp,setTemp] = useState('');
+  const handleTempChange = (newTemp) => {
+    setTemp(newTemp);
+  }
   return (
     <>
     {/* <Counter></Counter> */}
-    <CounterClass></CounterClass>
+    {/* <CounterClass></CounterClass> */}
+    {/* <SimpleForm></SimpleForm> */}
+    <TemperatureInput temp = {temp} handleTempChange = {handleTempChange}></TemperatureInput>
+    <TemperatureDisplay temp = {temp}></TemperatureDisplay>
     </>
   )
 }
